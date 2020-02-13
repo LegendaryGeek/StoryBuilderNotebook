74864b5edc768682b3f36ef2cb08db358588ce26
LegendaryGeek <timothydaley4@gmail.com>
2020-02-12 19:21:27 -0500

    first attempt at using juint

---
+++ src/test/java/geek/test/menus/MenuTest.java
@@ -0,0 +1,39 @@
+package geek.test.menus;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import geek.story.window.NotebookFrame;
+
+public class MenuTest {
+
+	static NotebookFrame frame = new NotebookFrame();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/geek/test/menus/NotebookFrameTest.java
@@ -0,0 +1,49 @@
+package geek.test.menus;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import geek.story.window.NotebookFrame;
+
+public class NotebookFrameTest {
+
+	static NotebookFrame tframe = new NotebookFrame();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetMainMenuePanel() {
+		assertNotNull(NotebookFrame.getMainMenuePanel());
+	}
+
+	@Test
+	public void testGetOptionsPanel() {
+		assertNotNull(NotebookFrame.getOptionsPanel());
+	}
+
+	@Test
+	public void testGetEditior() {
+		assertNull(tframe.getEditior());
+	}
+
+}
